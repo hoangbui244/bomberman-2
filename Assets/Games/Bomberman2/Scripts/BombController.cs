4bc6469666dd24499f150a76724dcd501b3be304
hoangbui244 <dotruongam@gmail.com>
2023-05-15 14:16:13 +0700

    *Hoàn thiện hiệu ứng bom nổ
    https://ac.bkb.network/projects/88?modal=Task-23606-88

--- Assets/Games/Bomberman2/Scripts/BombController.cs
+++ Assets/Games/Bomberman2/Scripts/BombController.cs
@@ -24,6 +24,19 @@ namespace io.lockedroom.Games.Bomberman2 {
         /// </summary>
         private int bombsRemaining;
         /// <summary>
+        /// Prefab
+        /// </summary>
+        public BombExplosion explosionPrefab;
+        /// <summary>
+        /// Thời gian hiệu ứng nổ tồn tại
+        /// </summary>
+        public float explosionDuration = 1f;
+        /// <summary>
+        /// Phạm vi bom nổ
+        /// Khi nhặt PowerUp thì phạm vi tăng
+        /// </summary>
+        public int explosionRadius = 1;
+        /// <summary>
         /// Hàm OnEnable để kích hoạt số bomb còn lại bằng số lượng bomb
         /// </summary>
         private void OnEnable() {
@@ -32,7 +45,7 @@ namespace io.lockedroom.Games.Bomberman2 {
         /// <summary>
         /// Hàm Update để update khi player ấn nút đặt bomb
         /// </summary>
-        private void Update() { 
+        private void Update() {
             // Kiểm tra xem số lượng bomb còn lại > 0 và gọi hàm PlaceBomb
             if (bombsRemaining > 0 && Input.GetKeyDown(inputKey)) {
                 StartCoroutine(PlaceBomb());
@@ -51,10 +64,40 @@ namespace io.lockedroom.Games.Bomberman2 {
             bombsRemaining--;
             // Đợi thời gian để bomb nổ xong (3s)
             yield return new WaitForSeconds(bombFuseTime);
+            // Reset lại vị trí quả bomb sau khi tác động physic để kèm hiệu ứng nổ tại vị trí mới
+            position = bomb.transform.position;
+            position.x = Mathf.Round(position.x);
+            position.y = Mathf.Round(position.y);
+            BombExplosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+            explosion.SetActiveRenderer(explosion.start);
+            // Xóa hiệu ứng nổ
+            explosion.DestroyAfter(explosionDuration);
+            // Gọi hàm Explode
+            Explode(position, Vector2.up, explosionRadius);
+            Explode(position, Vector2.down, explosionRadius);
+            Explode(position, Vector2.left, explosionRadius);
+            Explode(position, Vector2.right, explosionRadius);
             // Xóa bomb sau khi nổ
             Destroy(bomb);
             // Trả lại bombsRemaining
             bombsRemaining++;
+        }
+        /// <summary>
+        /// Hàm Explode để xử lý phạm vi hiệu ứng bom nổ trên nhiều tiles
+        /// </summary>
+        private void Explode(Vector2 position, Vector2 direction, int length) {
+            // Ktra nếu chiều dài <= 0  thì dừng
+            if (length <= 0) {
+                return;
+            }
+            position += direction;
+            BombExplosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+            // Xét điều kiện
+            explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
+            explosion.SetDirection(direction);
+            explosion.DestroyAfter(explosionDuration);
+            // Gọi hàm Explode với position mới
+            Explode(position, direction, length - 1);
         }
         /// <summary>
         /// Sau khi đặt bomb và ra khỏi quả bomb thì có thể tác động physic
---
+++ Assets/Games/Bomberman2/Scripts/BombExplosion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace io.lockedroom.Games.Bomberman2 {
+
+    public class BombExplosion : MonoBehaviour {
+        /// <summary>
+        /// hoạt ảnh bomb render (start)
+        /// </summary>
+        public AnimatedSpriteRenderer start;
+        /// <summary>
+        /// hoạt ảnh bomb render (middle)
+        /// </summary>
+        public AnimatedSpriteRenderer middle;
+        /// <summary>
+        /// hoạt ảnh bomb render (end)
+        /// </summary>
+        public AnimatedSpriteRenderer end;
+        /// <summary>
+        /// Hàm kích hoạt render hoạt ảnh
+        /// quyết định xem enable trạng thái nào của bomb nổ
+        /// </summary>
+        public void SetActiveRenderer(AnimatedSpriteRenderer renderer) {
+            start.enabled = renderer == start;
+            middle.enabled = renderer == middle;
+            end.enabled = renderer == end;
+        }
+        /// <summary>
+        /// Rotate hoạt ảnh bomb nổ ra các hướng
+        /// </summary>
+        public void SetDirection(Vector2 direction) {
+            // xoay hướng bomb nổ
+            float angle = Mathf.Atan2(direction.y, direction.x);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        /// <summary>
+        /// Hàm xóa gameObject (Làm gọn code)
+        /// </summary>
+        public void DestroyAfter(float seconds) { 
+            Destroy(gameObject, seconds);
+        }
+    }
+}
