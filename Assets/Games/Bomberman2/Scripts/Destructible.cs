c18c3cd2a1cd474fde5c08a13363b7a7b54c45e1
hoangbui244 <dotruongam@gmail.com>
2023-05-15 16:20:07 +0700

    *Vật phẩm rơi ra khi phá vật cản
    https://ac.bkb.network/projects/88?modal=Task-23608-88

--- Assets/Games/Bomberman2/Scripts/Destructible.cs
+++ Assets/Games/Bomberman2/Scripts/Destructible.cs
@@ -9,9 +9,27 @@ public class Destructible : MonoBehaviour
     /// </summary>
     public float destructionTime = 1f;
     /// <summary>
+    /// Tỉ lệ item xuất hiện khi phá vỡ gạch
+    /// </summary>
+    [Range(0f, 1f)]
+    public float itemSpawnChance = 0.2f;
+    /// <summary>
+    /// Vật phẩm nào có thể xuất hiện
+    /// </summary>
+    public GameObject[] spawnableItems;
+    /// <summary>
     /// Hàm Start
     /// </summary>
     private void Start() {
         Destroy(gameObject, destructionTime);
+    }
+    /// <summary>
+    /// Hàm khi phá vỡ vật cản thì rơi item
+    /// </summary>
+    private void OnDestroy() {
+        if (spawnableItems.Length > 0 && Random.value < itemSpawnChance) {
+            int randomIndex = Random.Range(0, spawnableItems.Length);
+            Instantiate(spawnableItems[randomIndex], transform.position, Quaternion.identity);
+        }
     }
 }
