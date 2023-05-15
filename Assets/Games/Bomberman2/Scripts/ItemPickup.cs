1edbcf81636f52f532e9a2a379903cdcc3b05c69
hoangbui244 <dotruongam@gmail.com>
2023-05-15 15:36:38 +0700

    *Nhặt vật phẩm
    https://ac.bkb.network/projects/88?modal=Task-23617-88

--- Assets/Games/Bomberman2/Scripts/ItemPickup.cs
+++ Assets/Games/Bomberman2/Scripts/ItemPickup.cs
@@ -1,18 +1,41 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemPickup : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class ItemPickup : MonoBehaviour {
+    /// <summary>
+    /// Danh sách loại vật phẩm
+    /// </summary>
+    public enum ItemType {
+        BlastRadius,
+        ExtraBomb,
+        SpeedUp,
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public ItemType type;
+    /// <summary>
+    /// Hàm để nhặt vật phẩm
+    /// </summary>
+    private void OnItemPickup(GameObject player) {
+        switch (type) {
+            case ItemType.BlastRadius:
+                //..
+                break;
+            case ItemType.ExtraBomb:
+                //..
+                break;
+            case ItemType.SpeedUp:
+                //..
+                break;
+        }
+        Destroy(gameObject);
+    }
+    /// <summary>
+    /// Hàm trigger khi tác động vào 1 item
+    /// </summary>
+    private void OnTriggerEnter2D(Collider2D other) {
+        // So sánh nếu vật có tag Player 
+        if (other.CompareTag("Player")) {
+            OnItemPickup(other.gameObject);
+        }
     }
 }
